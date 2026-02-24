75117c8bf75a903a758b156621e6b52540478d75
mukudanieru <111953203+mukudanieru@users.noreply.github.com>
2026-02-24 16:29:59 +0800

    refactor: migrate to async/await - refactor route handler from .then() chains to async/await

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,21 +1,19 @@
 const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
 
-blogsRouter.get("/", (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
+blogsRouter.get("/", async (request, response) => {
+  const blogs = await Blog.find({});
+
+  response.json(blogs);
 });
 
-blogsRouter.post("/", (request, response) => {
+blogsRouter.post("/", async (request, response) => {
   const { title, author, url, likes } = request.body;
 
   const blog = new Blog({ title, author, url, likes });
-  console.log(blog);
+  const savedBlog = await blog.save();
 
-  blog.save().then((savedBlog) => {
-    response.json(savedBlog);
-  });
+  response.json(savedBlog);
 });
 
 module.exports = blogsRouter;
