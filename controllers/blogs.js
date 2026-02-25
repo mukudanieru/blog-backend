fc779a08f7026ef16242fc71494cdf19441e9847
mukudanieru <111953203+mukudanieru@users.noreply.github.com>
2026-02-25 09:49:56 +0800

    feat: validate title and url when creating blogs

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -10,6 +10,20 @@ blogsRouter.get("/", async (request, response) => {
 blogsRouter.post("/", async (request, response) => {
   const { title, author, url, likes } = request.body;
 
+  if (!title) {
+    return response.status(400).json({
+      error: "MISSING_REQUIRED_FIELDS",
+      message: "title is required",
+    });
+  }
+
+  if (!url) {
+    return response.status(400).json({
+      error: "MISSING_REQUIRED_FIELDS",
+      message: "url is required",
+    });
+  }
+
   const blog = new Blog({ title, author, url, likes });
   const savedBlog = await blog.save();
 
