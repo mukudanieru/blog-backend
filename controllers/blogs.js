45700d5269d8633314f49f0f3ac22010963d4a58
mukudanieru <111953203+mukudanieru@users.noreply.github.com>
2026-02-28 14:03:30 +0800

    feat: implement blog update

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -70,3 +70,55 @@ blogsRouter.delete("/:id", async (request, response, next) => {
 });
 
 module.exports = blogsRouter;
+
+blogsRouter.put("/:id", async (request, response, next) => {
+  const { title, author, url } = request.body;
+
+  if (!title) {
+    return response.status(400).json({
+      error: "MISSING_REQUIRED_FIELDS",
+      message: "title is required",
+    });
+  }
+
+  if (!author) {
+    return response.status(400).json({
+      error: "MISSING_REQUIRED_FIELDS",
+      message: "author is required",
+    });
+  }
+
+  if (!url) {
+    return response.status(400).json({
+      error: "MISSING_REQUIRED_FIELDS",
+      message: "url is required",
+    });
+  }
+
+  const id = request.params.id;
+
+  try {
+    const blogToUpdate = await Blog.findById(id);
+
+    if (!blogToUpdate) {
+      return response.status(404).json({
+        error: "NOT_FOUND",
+        message: "blog post not found",
+      });
+    }
+
+    blogToUpdate.title = title;
+    blogToUpdate.author = author;
+    blogToUpdate.url = url;
+
+    try {
+      const updatedBlog = await blogToUpdate.save();
+
+      response.json(updatedBlog);
+    } catch (error) {
+      next(error);
+    }
+  } catch (error) {
+    next(error);
+  }
+});
