949bb9e1d7a1575370d73b1d3c42a1743921c614
mukudanieru <111953203+mukudanieru@users.noreply.github.com>
2026-03-01 15:32:32 +0800

    refactor: add new blogs with valid token only

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,6 +1,8 @@
+const jwt = require("jsonwebtoken");
 const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
 const User = require("../models/user");
+const helper = require("../utils/api_helper");
 
 blogsRouter.get("/", async (request, response) => {
   const blogs = await Blog.find({}).populate("user", { username: 1, name: 1 });
@@ -29,7 +31,18 @@ blogsRouter.get("/:id", async (request, response, next) => {
 });
 
 blogsRouter.post("/", async (request, response) => {
-  const { title, author, url, likes, userId } = request.body;
+  const { title, author, url, likes } = request.body;
+
+  const decodedToken = jwt.verify(
+    helper.getTokenFrom(request),
+    process.env.SECRET,
+  );
+
+  if (!decodedToken.id) {
+    return response
+      .status(401)
+      .json({ error: "INVALID_TOKEN", message: "token invalid" });
+  }
 
   if (!title) {
     return response.status(400).json({
@@ -45,7 +58,7 @@ blogsRouter.post("/", async (request, response) => {
     });
   }
 
-  const user = await User.findById(userId);
+  const user = await User.findById(decodedToken.id);
 
   if (!user) {
     return response.status(400).json({
